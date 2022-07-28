3e327ef9064c6bc98cbf80b9f939dc3130042174
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2022-07-28 21:04:23 +0200

    Primeros 3 ejercicios listos, pendiente de corregir el manejo de errores del #3

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,46 @@
+const testArr = [
+    {
+      title: 'Paths of Glory',
+      year: 1957,
+      director: 'Stanley Kubrick',
+      duration: '1h 28min',
+      genre: ['Drama', 'War'],
+      score: 8.4
+    },
+    {
+      title: 'Django Unchained',
+      year: 2012,
+      director: 'Quentin Tarantino',
+      duration: '2h 45min',
+      genre: ['Drama', 'Western'],
+      score: 8.4
+    }
+]
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    const directors = moviesArray.map((iterador) => iterador.director)
+    return directors
+}
+
+getAllDirectors(testArr)
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(movies) {
+    let answer = movies.filter((movie) => movie.director === "Steven Spielberg" && movie.genre.includes("Drama"));
+    return answer.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    const avgsum = moviesArray.reduce(function (acumulator, currentValue){
+        return acumulator + currentValue.score
+    },0 );
+    let total = avgsum/moviesArray.length
+    return Number(total.toFixed(2))
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
